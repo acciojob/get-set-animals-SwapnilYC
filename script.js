503cb23f0b6ede518211208d4e70904d0d99a1cc
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-11 20:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get getterForSpecies(){
+		return this.species;
+	}
+	makeSound(){
+		console.log(`a sound the animal makes`);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	super(species);
+	bark(){
+		console.log('woof');
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+super(species);
+	purr(){
+		console.log('purr');
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
